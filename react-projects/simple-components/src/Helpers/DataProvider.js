6ffa42249a3412b0748fa567336aeec7efa2fba5
bartoszsuchocki <bartosz.suchocki@enxoo.com>
2019-12-29 18:19:00 +0100

    Removed simple components example project

--- react-projects/simple-components/src/Helpers/DataProvider.js
+++
@@ -1,9 +0,0 @@
-const TODOS_URL = "https://jsonplaceholder.typicode.com/todos";
-function getTodoTasks(successHandler, errorHandler){
-    fetch(TODOS_URL)
-    .then(resp => {
-        return resp.json();
-    })
-    .then(successHandler,errorHandler);  
-}
-export {getTodoTasks};
